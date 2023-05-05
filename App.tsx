70f57769b5c6d0921524cc7453eb45165823f632
Hadi Rickit <rickithadi@gmail.com>
2023-05-05 22:37:52 +0800

    nav

--- App.tsx
+++ App.tsx
@@ -3,11 +3,14 @@ import { TailwindProvider } from "tailwind-rn";
 import LoginScreen from "@/screens/LoginScreen";
 
 import utilities from "./tailwind.json";
+import React from "react";
+import Navbar from "@/common/components/elements/NavBar";
 
 export default function App() {
   return (
     <TailwindProvider utilities={utilities}>
       <SafeAreaView>
+        <Navbar/>
         <LoginScreen />
       </SafeAreaView>
     </TailwindProvider>
--- src/common/components/elements/Navbar.tsx
+++ src/common/components/elements/Navbar.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import {  Text, View } from "react-native";
+import { Image } from "expo-image";
+import { Text, View } from "react-native";
 import { useTailwind } from "tailwind-rn";
 
+const bud = require("../../../../public/bud.svg") as string;
+const filters = require("../../../../public/filters.svg") as string;
 
 const Navbar = () => {
   const tailwind = useTailwind();
@@ -9,9 +12,19 @@ const Navbar = () => {
   return (
     <View
       style={tailwind(
-        "flex items-center justify-between flex-wrap bg-[#5A716A] p-6 h-3 "
-      )}>
-      <Text>hi</Text>
+        "flex items-center justify-between flex-row bg-[#5A716A] p-6 h-12 p-2 "
+      )}
+    >
+      <Image
+        source={bud}
+        style={tailwind("w-12 h-5")}
+        contentFit="contain"
+      ></Image>
+      <Image
+        source={filters}
+        style={tailwind("w-12 h-5")}
+        contentFit="contain"
+      ></Image>
     </View>
   );
 };
